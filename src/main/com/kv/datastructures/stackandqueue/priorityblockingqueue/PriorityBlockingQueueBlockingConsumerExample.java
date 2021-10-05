ee6312f8a55b760ac6b6bb718f1d1edef32ae359
Kunwar <kunwar.nitw@gmail.com>
2021-10-05 11:13:29 +0530

    Leetcode Challenges | Data Structures

---
+++ src/main/com/kv/datastructures/stackandqueue/priorityblockingqueue/PriorityBlockingQueueBlockingConsumerExample.java
@@ -0,0 +1,35 @@
+package main.com.kv.datastructures.stackandqueue.priorityblockingqueue;
+
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class PriorityBlockingQueueBlockingConsumerExample {
+    public static void main(String[] args) throws InterruptedException
+    {
+        PriorityBlockingQueue<Integer> priorityBlockingQueue = new PriorityBlockingQueue<>();
+        new Thread(() ->
+        {
+            System.out.println("Waiting to poll ...");
+            try
+            {
+                while(true)
+                {
+                    Integer poll = priorityBlockingQueue.take();
+                    System.out.println("Polled : " + poll);
+                    Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        Thread.sleep(TimeUnit.SECONDS.toMillis(2));
+        priorityBlockingQueue.add(1);
+
+        Thread.sleep(TimeUnit.SECONDS.toMillis(2));
+        priorityBlockingQueue.add(2);
+
+        Thread.sleep(TimeUnit.SECONDS.toMillis(2));
+        priorityBlockingQueue.add(3);
+    }
+}
